1ad56d306650b16b92c339a4dfe0bb935fa903f7
aladdin <aladdin.enbeh@gmail.com>
2025-09-29 12:17:06 +0300

    edit menu size

--- src/Pages/Menu.js
+++ src/Pages/Menu.js
@@ -2,19 +2,13 @@ import SpaloonMenu from "../assets/Pdf/SpaloonMenu.pdf";
 
 const Menu = () => {
   return (
-    // <div className="flex justify-center items-center  h-[700px] ">
-    //   <h1 className="text-4xl">Coming Soon </h1>
-    // </div>
-
-    <div className="w-full h-screen flex justify-center items-center">
-      <a
-        href={SpaloonMenu}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-white bg-black px-6 py-3 rounded-lg font-bold"
-      >
-        View Menu
-      </a>
+    <div className="w-full min-h-screen flex justify-center items-start pt-20">
+      <iframe
+        src={SpaloonMenu}
+        className="w-[90%] h-[80vh] rounded-lg shadow-lg"
+        frameBorder="0"
+        title="Spaloon Menu"
+      ></iframe>
     </div>
   );
 };
